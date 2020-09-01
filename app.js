31e686cb8e6278ab80a19c671da903dcfb864953
bweedop <kbweedop@gmail.com>
2020-09-01 07:49:57 -0600

    Resolve path issue in app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.static("build"));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
-app.use("/api/notes", notesRouter);
+app.use("/api/notes/", notesRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
